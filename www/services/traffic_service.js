d75ddff7b1af6405e4be457d036f8049c92a217f
Michael murray <michael.murray@hotmail.co.nz>
2015-09-23 22:18:22 +1200

    Added Traffic service and tidied up the index file
    Added Traffic service and tidied up the index file

---
+++ www/services/traffic_service.js
@@ -0,0 +1,15 @@
+/**
+ * Created by mike on 23/09/2015.
+ */
+angular.module("WAM").factory('traffic_service', function ($http) {
+
+
+
+    var selected_traffic_event = {};
+
+    return selected_traffic_event;
+
+
+
+
+})
